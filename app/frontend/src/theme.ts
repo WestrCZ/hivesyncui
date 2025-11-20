695a33f9222032536b65c6ca4714511cf9993cb3
WestrCZ <janekhujer@gmail.com>
2025-11-20 09:42:10 +0100

    fixed ELOOP for .env file on ./run compose up ; started to compose themes.

--- app/frontend/src/theme.ts
+++ app/frontend/src/theme.ts
@@ -1,4 +1,18 @@
 import { createTheme } from "@mui/material";
 
-const theme = createTheme();
+const theme = createTheme({
+  palette:
+  {
+    primary: {
+      dark: "#c6790c",
+      light: "#d49f30",
+      main: "#cc8613",
+    },
+    secondary: {
+      dark: "#474747",
+      light: "#a5a5a5",
+      main: "#7b7b7b",
+    },
+  },
+});
 export default theme;
